6674896254be32af163c26a5d7d13e9e6453e7ae
RHONT <rg915i@yandex.ru>
2023-07-11 11:13:29 +0300

    Initial commit

---
+++ src/Game.java
@@ -0,0 +1,222 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+public class Game extends JFrame { // класс Main в данной работе называют прямым наследником класса JFrame
+
+    /*При построении интерфейсов нужны компоненты-контерйнеры, которые будут содержать другие компоненты
+    пользовательского интерфейса. В Swing одним из таких компонентов-контейнеров является JPanel.
+    Класс GridLayout позволяет размещать компоненты в контейнере в виде таблицы. В каждой ячейке таблицы может быть размещен только один компонент.
+    Количество строк и столбцов таблицы определяется или в конструкторе, или вызовом методов setColumns и setRows.*/
+
+    private JPanel panel = new JPanel(new GridLayout(4, 4, 2, 2));
+
+    /*Главное меню JMenuBar - компонент графического интерфейса Java Swing*/
+
+    private JMenuBar menu = null;
+    //private JMenuBar help = null;
+    private final String fileMessage[] = new String [] {"Новая", "Выход"};
+    private final String helpMessage = "Об Авторе";
+    private static Random generator = new Random(); // генератор случайных чисел
+    private int[][] numbers = new int[4][4];
+
+    /* -=== Опредиление клиентской ширины экрана ===- */
+
+
+
+    public Game() {
+        setTitle("Пятнашки"); //Заголовок окна
+
+        setSize (300, 300); // Задаем размеры окна приложения
+        setLocationRelativeTo(null); // Окно приложения центрируется относительно экрана
+
+        setResizable(true); // задаем возможность растягивать окно
+        createMenu(); //инициализируем меню
+
+        setJMenuBar(menu); // добавляем панель меню к окну
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // закрываем программу при закрытии окна
+
+        //Класс container — прямой подкласс класса component, и наследует все его методы.
+        //Каждый компонент перед выводом на экран помещается в контейнер (container). Контейнер "знает", как разместить компоненты на экране.
+        /*Создав компонент — объект класса Component или его расширения, следует добавить его к предварительно созданному объекту класса container
+        или его расширения одним из методов add (). */
+
+        Container container = getContentPane();
+        init();
+        panel.setDoubleBuffered(true);
+        panel.setBackground(Color.red); // устанавливаем цвет фона
+        container.add(panel); // добавление компонентов в контейнер
+        repaintField();
+        JLabel();
+
+    }
+
+    public void init() { // описание метода init
+        int[] invariants = Tests.getShuffleArray();
+        int idx=0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers[i].length; j++) {
+                numbers[i][j]=invariants[idx];
+                idx++;
+            }
+        }
+    }
+
+    public void JLabel() {
+        Border solidBorder = BorderFactory.createLineBorder(Color.BLACK, 1); // создаем границу черного цвета
+        Font font = new Font("Verdana", Font.PLAIN, 12); // задаем тип шрифта, и его размер
+        JLabel topLabel = new JLabel(); // создаем обьект topLabel типа JLabel
+        topLabel.setBorder(solidBorder); // устанавливаем границу
+        topLabel.setFont(font); // устанавливаем тип текста
+        topLabel.setForeground(Color.RED); // Устанавливаем цвет текста
+        menu.add(topLabel); // добавляем JLabel на пенель menu
+    }
+
+
+
+    public void repaintField() {  //метод расстановки кнопок со значениями на сетке
+        panel.removeAll();
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                JButton button = new JButton(Integer.toString(numbers[i][j]));
+                button.setFocusable(false);
+                panel.add(button);
+                button.setBackground(Color.getHSBColor(0.1059322f, 0.5221239f, 0.8862745f)); // устанавливаем цвет кнопок
+                if (numbers[i][j] == 0) {
+                    button.setVisible(false); // сокрытие нулевого элемента массива
+                } else
+                    button.addActionListener(new ClickListener());
+            }
+        }
+
+        panel.validate();
+    }
+
+    public boolean checkWin() { //метод проверки победы
+        boolean status = true;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (i == 3 && j > 2) //проверка на то, что последняя ячейка в сетке пустая
+                    break;
+                if (numbers[i][j] != i * 4 + j + 1) { //проверка на соотвествие элементам массива координатам в сетке
+                    status = false;
+                }
+            }
+        }
+        return status;
+    }
+
+    private void createMenu() {
+        menu = new JMenuBar();
+        NewMenuListener listener=new NewMenuListener();
+
+        JMenu fileMenu = new JMenu("File");
+        JMenuItem item_1 = new JMenuItem("New");
+        item_1.setActionCommand("new");
+        item_1.addActionListener(listener);
+
+
+        JMenuItem item_2 = new JMenuItem("Exit");
+        item_2.addActionListener(listener);
+        item_2.setActionCommand("exit");
+        item_2.addActionListener(listener);
+
+        fileMenu.add(item_1);
+        fileMenu.add(new JSeparator());
+        fileMenu.add(item_2);
+
+        menu.add(fileMenu);
+
+        JMenu helpMenu = new JMenu("Help");
+        JMenuItem item_3 = new JMenuItem("About author");
+        item_3.setActionCommand("about");
+        item_3.addActionListener(listener);
+        helpMenu.add(item_3);
+        menu.add(helpMenu);
+    }
+
+    private class NewMenuListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String command = e.getActionCommand();
+            if ("exit".equals(command)) {
+                System.exit(0);
+            }
+            if ("about".equals(command)) {
+                JOptionPane.showMessageDialog(null, "Белова Анастасия\nГруппа 111\n2023 год");
+            }
+            if ("new".equals(command)) {
+                init();
+                repaintField();
+            }//
+        }
+    }
+
+    private class ClickListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            JButton button = (JButton) e.getSource();
+            button.setVisible(false);
+            String name = button.getText();
+            change(Integer.parseInt(name));
+        }
+    }
+
+    public void change(int num) { // передаем в качестве входящих параметров метода change переменную num типа int
+        int i = 0, j = 0; // присваиваем переменным i и j типа int значение равное 0
+        for (int k = 0; k < 4; k++) { // перебираем элементы k от 0 до 3
+            for (int l = 0; l < 4; l++) { // перебираем элементы l от 0 до 3
+                if (numbers[k][l] == num) { // если массив numbers[k][l] равен переменной num то,
+                    i = k; // переменную i приравниваем переменной k
+                    j = l; // переменную j приравниваем переменной l
+                }
+            }
+        }
+
+        /*реализация логики сдвигов кнопок на сетке 4 Х 4*/
+        //сдвиг вверх по строкам
+        if (i > 0) { // условие отвечающее за то можно ли сдвинуть кнопку по строке
+            if (numbers[i - 1][j] == 0) { //сравниваем значение координат элемента массива с кнопкой которая в текущем массиве равна нулю
+                numbers[i - 1][j] = num; //присваиваем переменной num значение координат элемента массива
+                numbers[i][j] = 0; //присваиваем нулевой элемент массива в ячейку которая перед этим смещалась в ноль
+            }
+        }
+        //сдвиг вниз по строкам
+        if (i < 3) {
+            if (numbers[i + 1][j] == 0) {
+                numbers[i + 1][j] = num;
+                numbers[i][j] = 0;
+            }
+        }
+        //сдвиг влево по столбцам
+        if (j > 0) {
+            if (numbers[i][j - 1] == 0) {
+                numbers[i][j - 1] = num;
+                numbers[i][j] = 0;
+            }
+        }
+        //сдвиг вправо по столбцам
+        if (j < 3) {
+            if (numbers[i][j + 1] == 0) {
+                numbers[i][j + 1] = num;
+                numbers[i][j] = 0;
+            }
+        }
+        repaintField();
+        if (checkWin()) {
+            JOptionPane.showMessageDialog(null, "ВЫ ВЫИГРАЛИ!", "Поздравляем", 1);
+            init();
+            repaintField();
+            setVisible(false);
+            setVisible(true);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(1000);
+        JFrame app = new Game();
+        app.setVisible(true);
+    }
+}
---
+++ src/Tests.java
@@ -0,0 +1,35 @@
+
+import java.util.Random;
+
+public class Tests {
+
+    public static int[] getShuffleArray(){
+        int[] arr=rowArray();
+        shuffle(arr);
+        return arr;
+    }
+
+    private static int[] rowArray(){
+        int[] arr=new int[16];
+        for (int i=0;i<15;i++) {
+            arr[i]=i+1;
+        }
+        return arr;
+    }
+
+    private static void shuffle(int[] inputArr){
+        int temp;
+        int buffer;
+        int randomIdx;
+        Random random=new Random();
+        for (int i = 0; i < inputArr.length-1; i++) {
+            randomIdx=random.nextInt(15);
+
+            temp=inputArr[randomIdx];
+            buffer=inputArr[i];
+
+            inputArr[i]=temp;
+            inputArr[randomIdx]=buffer;
+        }
+    }
+}
